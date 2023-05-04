da2d33f30dab4e26abd2ce2c21ddde2de3c89dfe
Oyukawa <kykw.6.swimming@gmail.com>
2023-05-04 16:11:01 +0900

    feat:base setup

---
+++ src_s3n/src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-btn color="success">This is about button</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col> <h1>This is about page</h1> </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
