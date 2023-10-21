55d41847cc8fd3425164327cb8c574aeac5721bd
Oyukawa <kykw.6.swimming@gmail.com>
2023-10-21 22:36:57 +0900

    ドメイン修正＆dayjs追加

--- src_s3n/vite.config.ts
+++ src_s3n/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/s3n/',
+  base: '/yuk4wa/',
   plugins: [vue()],
   resolve: {
     alias: {
